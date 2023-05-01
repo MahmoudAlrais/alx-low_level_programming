a8b62584d0d02b71fd10004ddee005576974aa57
MahmoudAlrais <mahmoudalraismerch@gmail.com>
2023-05-01 11:52:12 -0700

    update 17

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	for (h = 0; temp && h < idx; i++)
+	for (h = 0; temp && h < idx; h++)
 	{
 		if (h == idx - 1)
 		{

d5e536b9fc0b9def4be588811f90768ed4cb9dda
MahmoudAlrais <mahmoudalraismerch@gmail.com>
2023-05-01 11:41:23 -0700

    update 8

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * free_listint - frees a linked list
- * @head: listint_t list to be freed
+ * free_listint - frees the linked list
+ * @head: listint_t list to freed
  */
 void free_listint(listint_t *head)
 {

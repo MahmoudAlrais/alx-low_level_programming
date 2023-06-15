c955a381ed0fb2d0d99c1fc2b3bf40be3135d99a
MahmoudAlrais <mahmoudalraismerch@gmail.com>
2023-06-15 08:46:13 -0700

    0x17-doubly_linked_lists 2

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a
+ * print_dlistint - prints all elements of a
  * dlistint_t list
  *
- * @h: head of the list
- * Return: the number of nodes
+ * @h: head of list
+ * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -7,8 +7,8 @@
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
- * @prev: points to the previous node
- * @next: points to the next node
+ * @prev: points to previous node
+ * @next: points to next node
  *
  * Description: doubly linked list node structure
  * for Holberton project
